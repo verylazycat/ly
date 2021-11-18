600447320aadaa4fdeaf3bc0844d2d2d5c13ba9b
verylazycat <1647495684@qq.com>
2021-11-18 13:10:49 +0800

    logging check

---
+++ include/Logging.h
@@ -0,0 +1,18 @@
+//
+// Created by admin233 on 2021/11/18.
+//
+
+#ifndef LY_LOGGING_H
+#define LY_LOGGING_H
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/basic_file_sink.h>
+#include "Utils.h"
+using namespace std;
+class Logging {
+public:
+// https://blog.csdn.net/cainiao17441898/article/details/121192554
+// https://cloud.tencent.com/developer/article/1459390
+
+};
+
+#endif //LY_LOGGING_H
---
+++ src/Logging.cpp
@@ -0,0 +1,5 @@
+//
+// Created by admin233 on 2021/11/18.
+//
+
+#include "Logging.h"
