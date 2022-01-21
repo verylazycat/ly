e3a84b0042626dd2d6d8e595ed6329bc0abd564a
verylazycat <1647495684@qq.com>
2022-01-21 21:52:12 +0800

    gflags

--- main.cpp
+++ main.cpp
@@ -25,89 +25,128 @@
 #include <fstream> 
 #include <iomanip>
 #include <string>
+#include <gflags/gflags.h>
 using namespace std;
 #define MAXDATABUFF 1024
 #define MD5LENTH 16
+DEFINE_bool(default_scan, true, "default scan,exclude virus scan");
+DEFINE_bool(ac,false,"accounting scan");
+DEFINE_bool(au,false,"auth scan");
+DEFINE_bool(cr,false,"crypto scan");
+DEFINE_bool(da,false,"database scan");
+DEFINE_bool(pr,false,"printing scan");
+static std::string g_version;
+static std::string g_help;
+//========== version define ==========
+std::string& getVersion() {
+    g_version = "1.0.0";
+    return g_version;
+    }
+//========== help info define ==========
+std::string& getHelp() {
+    g_help = "help info";
+    return g_help;
+    }
 int main(int argc ,char **argv) {
+    //========== init gflags ==========
+
+    google::SetVersionString(getVersion());
+    google::SetUsageMessage(getHelp());
+    google::ParseCommandLineFlags(&argc, &argv, true);
+    
+    //========== init core function object ==========
+    
     Accounting accounting;
     Auth auth;
     Boot boot;
     Crypto crypto;
-    accounting.BaseInfo();
-    
-    auth.CheckIsOnlyUser();
-    auth.CheckNoPwUser();
-    auth.CheckUmask();
-    
-    boot.CheckGrubBootLoader();
-    boot.CheckAuthBoot();
-    boot.CheckCron();
-
-    crypto.CheckCerts();
     Database database;
-    database.CheckNullPass();
-    database.CheckDangerousCMD();
-    database.CheckRedisPass();
-    database.CheckIntranetAccess();
-
     Printing printing;
-    printing.CheckCUPSPermissions();
-    printing.CheckCUPSRemoteAccess();
-
     Logging logging;
-    logging.AuditConfiguration();
-
     Nameservers nameservers;
-    nameservers.CheckDNS();
-
     SSH ssh;
-    ssh.AuditSSHConfig();
-
     Tools tools;
-    tools.CheckFailBanConfig();
-
     Processes processes;
-    processes.RetrievingZombieProcesses();
-
     Web web;
-    web.CheckNginxSSL();
-    web.CheckNginxLog();
-
-    // Malware malware;
-    // malware.UpdateDataBase();
-    // malware.BinScan();
-
+    Malware malware;// more time to scan
     Kernel kernel;
-    kernel.CheckPAE();
-    kernel.CheckCoreDumpOK();
-
     NetWorking netWorking;
-    netWorking.HostsParsing();
-    netWorking.NicStatus();
-    netWorking.ss();
-    netWorking.arp();
-
-    FileIntegrity fileintegrity;
-    fileintegrity.CheckNecessaryBlock();
-    fileintegrity.CheckSwap();
-    fileintegrity.CheckTmp();
-    fileintegrity.CheckAIDE();
-    fileintegrity.TmpStickybit();
-    
+    FileIntegrity fileintegrity;    
     Framework framework;
-    framework.apparmor_status();
-    // Utils::updatebyip("LY-core","BaseInfo","sysrelease","1234");
-    // Utils::updatebyip("LY-core","BaseInfo","bufferram",999);
-
     Firewall firewall;
-    firewall.GetIptabelse();
-
     Hardening hardening;
-    hardening.CheckGCC();
-    hardening.CheckGPP();
-    hardening.CheckCMAKE();
-
     Squid squid;
-    squid.CheckSquidStatus();
+    //========== gflags setting ==========
+    ///========== default init ==========
+    if (FLAGS_ac || FLAGS_au || FLAGS_cr || FLAGS_da ||FLAGS_pr){
+        FLAGS_default_scan = !FLAGS_default_scan;
+    }
+    ///========== default scan ==========
+    if (FLAGS_default_scan){
+        accounting.BaseInfo();
+        auth.CheckIsOnlyUser();
+        auth.CheckNoPwUser();
+        auth.CheckUmask();
+        boot.CheckAuthBoot();
+        boot.CheckCron();
+        boot.CheckGrubBootLoader();
+        crypto.CheckCerts();
+        database.CheckDangerousCMD();
+        database.CheckIntranetAccess();
+        database.CheckNullPass();
+        database.CheckRedisPass();
+        printing.CheckCUPSPermissions();
+        printing.CheckCUPSRemoteAccess();
+        logging.AuditConfiguration();
+        logging.AuditLog();
+        nameservers.CheckDNS();
+        ssh.AuditSSHConfig();
+        // tools.CheckFailBanConfig();
+        processes.RetrievingZombieProcesses();
+        web.CheckNginxLog();
+        web.CheckNginxSSL();
+        kernel.CheckCoreDumpOK();
+        kernel.CheckCPUINIFO();
+        kernel.CheckPAE();
+        kernel.CheckSYSCTL();
+        kernel.CheckSysCtlValue();
+        netWorking.arp();
+        netWorking.HostsParsing();
+        netWorking.NicStatus();
+        netWorking.ss();
+        fileintegrity.CheckAIDE();
+        fileintegrity.CheckNecessaryBlock();
+        fileintegrity.CheckSwap();
+        fileintegrity.CheckTmp();
+        fileintegrity.TmpStickybit();
+        framework.apparmor_status();
+        hardening.CheckCMAKE();
+        hardening.CheckGCC();
+        hardening.CheckGPP();
+        squid.CheckSquidStatus();
+    }
+    ///========== user setting  ==========
+    if (FLAGS_ac){
+        accounting.BaseInfo();
+    }
+    if (FLAGS_au){
+        auth.CheckIsOnlyUser();
+        auth.CheckNoPwUser();
+        auth.CheckUmask();
+    }
+    if (FLAGS_cr){
+        crypto.CheckCerts();
+    }
+    if (FLAGS_da){
+        database.CheckDangerousCMD();
+        database.CheckIntranetAccess();
+        database.CheckNullPass();
+        database.CheckRedisPass();
+    }
+    if (FLAGS_pr){
+        printing.CheckCUPSPermissions();
+        printing.CheckCUPSRemoteAccess();
+    }
+    
     return 0;
 }
